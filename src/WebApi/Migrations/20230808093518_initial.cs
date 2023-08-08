789e5fb5e7a8f9a6c4b64547381cfb2403e5ee7a
Rodrigo Losik <rodrigolosik@gmail.com>
2023-08-08 11:11:51 +0100

    Initial commit

---
+++ src/WebApi/Migrations/20230808093518_initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Sku_Value = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ src/WebApi/Products/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Core.Commands;
+using WebApi.Core.Entities;
+using WebApi.Core.Queries;
+using WebApi.Storage;
+
+namespace WebApi.Products;
+
+internal static class Configuration
+{
+    public static IServiceCollection AddProductServices(this IServiceCollection services)
+        => services
+            .AddQueryable<Product, WarehouseDBContext>()
+            .AddCommandHandler<RegisterProduct, HandleRegisterProduct>(s =>
+            {
+                var dbContext = s.GetRequiredService<WarehouseDBContext>();
+                return new HandleRegisterProduct(dbContext.AddAndSave, dbContext.ProductWithSKUExists);
+            })
+            .AddQueryHandler<GetProducts, IReadOnlyList<ProductListItem>, HandleGetProducts>()
+            .AddQueryHandler<GetProductDetails, ProductDetails?, HandleGetProductDetails>();
+
+    public static void SetupProductsModel(this ModelBuilder modelBuilder)
+    => modelBuilder.Entity<Product>()
+    .OwnsOne(p => p.Sku);
+
+    public static ValueTask<bool> ProductWithSKUExists(this WarehouseDBContext dbContext, SKU productSKU, CancellationToken ct)
+        => new(dbContext.Set<Product>().AnyAsync(product => product.Sku.Value == productSKU.Value, ct));
+}
